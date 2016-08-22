bcef27d3c1fcded6fac377d1b234cede3b8d458e
Jena SanCartier <jenasancartier@gmail.com>
2016-08-22 14:00:53 -0700

    initial commit. also add forms in index.html. start business logic (creating a constructor function for Destination)

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//business logic
+function Destination(destination, location, landmark, when, notes) {
+  this.destination = destination;
+  this.location = location;
+  this.landmark = landmark;
+  this.whenVisited = when;
+  this.notes = notes;
+}
+
+
+
+//user interface logic
