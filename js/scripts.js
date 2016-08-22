fdfa1bb98231384093cf8c8ea48743640807c7c3
Jena SanCartier <jenasancartier@gmail.com>
2016-08-22 14:25:30 -0700

    write javascript user interface logic for inputted values and creating newDestinations with user input for the Destination constructor

--- js/scripts.js
+++ js/scripts.js
@@ -8,5 +8,21 @@ function Destination(destination, location, landmark, when, notes) {
 }
 
 
-
 //user interface logic
+$(document).ready(function(){
+  $("form#new-place").submit(function(event){
+    event.preventDefault();
+
+    var inputtedDestination = $("input#new-destination").val()
+    var inputtedLocation = $("input#new-location").val();
+    var inputtedLandmark = $("input#new-landmark").val();
+    var inputtedWhen = $("input#new-time").val();
+    var inputtedNotes = $("input#new-notes").val();
+
+    var newDestination = new Destination(inputtedDestination, inputtedLocation, inputtedLandmark, inputtedWhen, inputtedNotes);
+
+    $("ul#destinations").append("<li><span class='destination'>" + newDestination.destination + "</span></li>");
+
+
+  })
+})
