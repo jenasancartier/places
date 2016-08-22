49db118cd8fdc42ce7f0ed97b1e0d1bc063d6049
Jena SanCartier <jenasancartier@gmail.com>
2016-08-22 14:55:37 -0700

    complete front end javascript to display destination object details corresponding to whatever appended destination that was clicked on

--- js/scripts.js
+++ js/scripts.js
@@ -13,16 +13,30 @@ $(document).ready(function(){
   $("form#new-place").submit(function(event){
     event.preventDefault();
 
+    //gives usuable names to various user inputs
     var inputtedDestination = $("input#new-destination").val()
     var inputtedLocation = $("input#new-location").val();
     var inputtedLandmark = $("input#new-landmark").val();
     var inputtedWhen = $("input#new-time").val();
     var inputtedNotes = $("input#new-notes").val();
 
+    //adds new object data based on user input variables made
     var newDestination = new Destination(inputtedDestination, inputtedLocation, inputtedLandmark, inputtedWhen, inputtedNotes);
 
     $("ul#destinations").append("<li><span class='destination'>" + newDestination.destination + "</span></li>");
 
+    //.destination corresponds to li span class above
+    $(".destination").last().click(function() {
+      //after you click on the destination ul class, the show-destination id that is currently hidden in the html, will show the following information
+      $("#show-destination").show();
+      $("#show-destination h2").text(newDestination.destination);
+      $(".destination-name").text(newDestination.destination);
+      $(".location-name").text(newDestination.location);
+      $(".landmark-name").text(newDestination.landmark);
+      $(".when-visited").text(newDestination.when);
+      $(".notes").text(newDestination.notes);      
+    });
 
-  })
-})
+
+  });
+});
